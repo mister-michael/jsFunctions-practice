75d9750a94c37f32778ebed26f3a1696a1d1886a
Michael Clark <michaelclarknashville@protonmail.com>
2020-01-17 13:54:33 -0600

    completed cookout

--- main.js
+++ main.js
@@ -1,25 +1,49 @@
-
-
-// A local nightclub is having a Battle of the Bands night in a few months. 
-// They expect many bands to sign up because the grand prize is $20,000. 
-// To make things easier on the nightclub management, they want each band to be assigned a 
-// number so that they can easily keep track of the order in which the bands will perform.
-
-// Your job is to write a function that accepts any band name as an argument. 
-// The function will increment a global variable by one each time it is invoked, 
-// and return that number, and the band name concatenated together.
-
-let bandNumber = 1
-
-const takeNumber = function (bandName) {
-    
-    return bandNumber++ + ". " + bandName;
-    
-
+const hamburger = {
+	name: 'Hamburger',
+	type: 'beef',
+	cooked: false,
+}
+const zucchini = {
+	name: 'Zucchini',
+	type: 'vegetable',
+	cooked: false,
+}
+const chickenBreast = {
+	name: 'Chicken Breast',
+	type: 'chicken',
+	cooked: false,
+}
+const corn = {
+	name: 'Corn',
+	type: 'vegetable',
+	cooked: false,
+}
+const steak = {
+	name: 'Steak',
+	type: 'beef',
+	cooked: false,
 }
 
-const scum = takeNumber("Galactic Scum")
-console.log(scum)  // This should print "1. Galactic Scum" in the console
+// An array that is grouping the objects together.
+const foods = [hamburger, zucchini, chickenBreast, corn, steak];
 
-const under = takeNumber("Underdogs")
-console.log(under)  // This should print "2. Underdogs" in the console
+// An empty array that will store the objects after the `grill()` function cooks the food.
+const cookedFood = [];
+
+function grill (currentObject) {
+    
+     
+       // Modify the food so that it is cooked
+    currentObject.cooked = true; 
+    
+    
+    cookedFood.push(currentObject);
+    // Put the cooked food into the appropriate array
+    
+};
+
+for (let i = 0; i < foods.length; i++) {
+    grill(foods[i]);
+}
+
+console.log(cookedFood);
